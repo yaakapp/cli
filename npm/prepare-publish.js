df236d66e2614a5848c6c71a114f22019e290223
Gregory Schier <gschier1990@gmail.com>
2024-09-09 09:38:20 -0700

    Fix paths

--- npm/prepare-publish.js
+++ npm/prepare-publish.js
@@ -1,18 +1,19 @@
 const {copyFileSync, readFileSync, writeFileSync} = require("node:fs");
+const {join} = require("node:path");
 
 console.log("Copying binary files to packages")
-copyFileSync('dist/cli_darwin_arm64/yaakcli', 'npm/cli-darwin-x64/bin/yaakcli');
-copyFileSync('dist/cli_darwin_amd64_v1/yaakcli', 'npm/cli-darwin-arm64/bin/yaakcli');
-copyFileSync('dist/cli_linux_amd64_v1/yaakcli', 'npm/cli-linux-x64/bin/yaakcli');
-copyFileSync('dist/cli_windows_amd64_v1/yaakcli.exe', 'npm/cli-win32-x64/bin/yaakcli.exe');
+copyFileSync(join(__dirname, '../dist/cli_darwin_arm64/yaakcli'), join(__dirname, 'cli-darwin-x64/bin/yaakcli'));
+copyFileSync(join(__dirname, 'dist/cli_darwin_amd64_v1/yaakcli'), join(__dirname, 'cli-darwin-arm64/bin/yaakcli'));
+copyFileSync(join(__dirname, 'dist/cli_linux_amd64_v1/yaakcli'), join(__dirname, 'cli-linux-x64/bin/yaakcli'));
+copyFileSync(join(__dirname, 'dist/cli_windows_amd64_v1/yaakcli.exe'), join(__dirname, 'cli-win32-x64/bin/yaakcli.exe'));
 
 const version = process.env.YAAK_CLI_VERSINO
 console.log(`Setting package versions to ${version}`);
-replacePackageVersion('npm/cli', version);
-replacePackageVersion('npm/cli-darwin-arm64', version);
-replacePackageVersion('npm/cli-darwin-x64', version);
-replacePackageVersion('npm/cli-linux-x64', version);
-replacePackageVersion('npm/cli-win32-x64', version);
+replacePackageVersion(join(__dirname, 'cli'), version);
+replacePackageVersion(join(__dirname, 'npm/cli-darwin-arm64'), version);
+replacePackageVersion(join(__dirname, 'npm/cli-darwin-x64'), version);
+replacePackageVersion(join(__dirname, 'npm/cli-linux-x64'), version);
+replacePackageVersion(join(__dirname, 'npm/cli-win32-x64'), version);
 
 console.log("Done preparing for publish");
 
