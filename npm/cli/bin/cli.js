59ea0a0b43eb5cd11a93f82c6a5e4be34106e449
Gregory Schier <gschier1990@gmail.com>
2024-09-09 11:22:50 -0700

    Fix binary path during exec

--- npm/cli/bin/cli.js
+++ npm/cli/bin/cli.js
@@ -2,24 +2,12 @@
 
 const path = require("path");
 const childProcess = require("child_process");
-const {BINARY_NAME} = require("../common");
-
-// Lookup table for all platforms and binary distribution packages
-const BINARY_DISTRIBUTION_PACKAGES = {
-  darwin: "npm-binary-example-darwin",
-  linux: "npm-binary-example-linux",
-  freebsd: "npm-binary-example-linux",
-  win32: "npm-binary-example-win32",
-};
-
-// Determine package name for this platform
-const platformSpecificPackageName =
-  BINARY_DISTRIBUTION_PACKAGES[process.platform];
+const {BINARY_NAME, PLATFORM_SPECIFIC_PACKAGE_NAME} = require("../common");
 
 function getBinaryPath() {
   try {
     // Resolving will fail if the optionalDependency was not installed
-    return require.resolve(`${platformSpecificPackageName}/bin/${BINARY_NAME}`);
+    return require.resolve(`${PLATFORM_SPECIFIC_PACKAGE_NAME}/bin/${BINARY_NAME}`);
   } catch (e) {
     return path.join(__dirname, "..", BINARY_NAME);
   }
